eb6bb4c653d9a9e2fb2879916e184353dcfe98b6
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-08-21 19:40:39 +0900

    add template

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,15 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"html/template"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,7 +23,24 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		// mapの使用例
+		data := map[string]string{
+			"ARG1": "A1",
+			"ARG2": "A2",
+		}
+
+		// New(<ファイル名>).ParseFiles(<ひな形ファイル>)
+		t, err := template.New("message.tmpl").ParseFiles("template/message.tmpl")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Execute(io.Writer(出力先), <データ>)
+		if err = t.Execute(os.Stdout, data); err != nil {
+			log.Fatal(err)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -47,5 +63,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
